071765966fe6c0a2b38695aa5aa3a2ae986087f8
saewoomk88 <43767934+saewoomk88@users.noreply.github.com>
2020-01-21 00:57:50 +0900

    fileDTO

---
+++ src/main/java/com/iu/file/FileDTO.java
@@ -0,0 +1,37 @@
+package com.iu.file;
+
+public class FileDTO {
+	
+	private int fnum;
+	private String pname;
+	private String fname;
+	private String oname;
+	
+	
+	public int getFnum() {
+		return fnum;
+	}
+	public void setFnum(int fnum) {
+		this.fnum = fnum;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getOname() {
+		return oname;
+	}
+	public void setOname(String oname) {
+		this.oname = oname;
+	}
+	
+
+}
